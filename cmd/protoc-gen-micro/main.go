97f75cfadbbff26ce32e41bd9e134f35699faffc
Henry Sha <henry.sha@playcubegames.com>
2020-12-24 18:38:21 +0800

    Remove unused test code from protoc-gen-micro

--- cmd/protoc-gen-micro/main.go
+++ cmd/protoc-gen-micro/main.go
@@ -3,138 +3,15 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path"
 	"strings"
-	"text/tabwriter"
 
 	"github.com/golang/protobuf/proto"
-	descriptor "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
 	descriptorpb "google.golang.org/protobuf/types/descriptorpb"
 )
-
-type GoExample struct {
-	Request    *plugin.CodeGeneratorRequest
-	Response   *plugin.CodeGeneratorResponse
-	Parameters map[string]string
-}
-
-type LocationMessage struct {
-	Location        *descriptor.SourceCodeInfo_Location
-	Message         *descriptor.DescriptorProto
-	LeadingComments []string
-}
-
-func (runner *GoExample) PrintParameters(w io.Writer) {
-	const padding = 3
-	tw := tabwriter.NewWriter(w, 0, 0, padding, ' ', tabwriter.TabIndent)
-	fmt.Fprintf(tw, "Parameters:\n")
-	for k, v := range runner.Parameters {
-		fmt.Fprintf(tw, "%s:\t%s\n", k, v)
-	}
-	fmt.Fprintln(tw, "")
-	tw.Flush()
-}
-
-func (runner *GoExample) getLocationMessage() map[string][]*LocationMessage {
-
-	ret := make(map[string][]*LocationMessage)
-	for index, filename := range runner.Request.FileToGenerate {
-		locationMessages := make([]*LocationMessage, 0)
-		proto := runner.Request.ProtoFile[index]
-		desc := proto.GetSourceCodeInfo()
-		locations := desc.GetLocation()
-		for _, location := range locations {
-			// I would encourage developers to read the documentation about paths as I might have misunderstood this
-			// I am trying to process message types which I understand to be `4` and only at the root level which I understand
-			// to be path len == 2
-			if len(location.GetPath()) > 2 {
-				continue
-			}
-
-			leadingComments := strings.Split(location.GetLeadingComments(), "\n")
-			if len(location.GetPath()) > 1 && location.GetPath()[0] == int32(4) {
-				message := proto.GetMessageType()[location.GetPath()[1]]
-				println(message.GetName())
-				locationMessages = append(locationMessages, &LocationMessage{
-					Message:  message,
-					Location: location,
-					// Because we are only parsing messages here at the root level we will not get field comments
-					LeadingComments: leadingComments[:len(leadingComments)-1],
-				})
-			}
-		}
-		ret[filename] = locationMessages
-	}
-	return ret
-}
-
-func (runner *GoExample) CreateMarkdownFile(filename string, messages []*LocationMessage) error {
-	// Create a file and append it to the output files
-
-	var outfileName string
-	var content string
-	outfileName = strings.Replace(filename, ".proto", ".md", -1)
-	var mdFile plugin.CodeGeneratorResponse_File
-	mdFile.Name = &outfileName
-	var buf bytes.Buffer
-	buf.WriteString(fmt.Sprintf("# %s\n", outfileName))
-	for _, locationMessage := range messages {
-		buf.WriteString(fmt.Sprintf("\n## %s\n", locationMessage.Message.GetName()))
-		buf.WriteString(fmt.Sprintf("### %s\n", "Leading Comments"))
-		for _, comment := range locationMessage.LeadingComments {
-			buf.WriteString(fmt.Sprintf("%s\n", comment))
-		}
-		if len(locationMessage.Message.NestedType) > 0 {
-			buf.WriteString(fmt.Sprintf("### %s\n", "Nested Messages"))
-			for _, nestedMessage := range locationMessage.Message.NestedType {
-				buf.WriteString(fmt.Sprintf("#### %s\n", nestedMessage.GetName()))
-				buf.WriteString(fmt.Sprintf("#### %s\n", "Fields"))
-				for _, field := range nestedMessage.Field {
-					buf.WriteString(fmt.Sprintf("%s - %s\n", field.GetName(), field.GetLabel()))
-				}
-			}
-		}
-		for _, field := range locationMessage.Message.Field {
-			buf.WriteString(fmt.Sprintf("%s - %s\n", field.GetName(), field.GetLabel()))
-		}
-	}
-	content = buf.String()
-	mdFile.Content = &content
-	runner.Response.File = append(runner.Response.File, &mdFile)
-	os.Stderr.WriteString(fmt.Sprintf("Created File: %s", filename))
-	return nil
-}
-
-func (runner *GoExample) generateMessageMarkdown() error {
-	// This convenience method will return a structure of some types that I use
-	fileLocationMessageMap := runner.getLocationMessage()
-	for filename, locationMessages := range fileLocationMessageMap {
-		runner.CreateMarkdownFile(filename, locationMessages)
-	}
-	return nil
-}
-
-func (runner *GoExample) generateCode() error {
-	// Initialize the output file slice
-	files := make([]*plugin.CodeGeneratorResponse_File, 0)
-	runner.Response.File = files
-
-	{
-		err := runner.generateMessageMarkdown()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-type serviceDescriptor struct {
-	Name string
-}
 
 type microGenerator struct {
 	req    *plugin.CodeGeneratorRequest
